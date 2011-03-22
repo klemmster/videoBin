fbc8265215b040c13c4256f670583c3cd33557d9
Richard Klemm <klemmster@gmail.com>
2011-03-22 15:47:26 +0100

    Fix mp4 Encoding

--- lib/paperclip_processors/video_converter.rb
+++ lib/paperclip_processors/video_converter.rb
@@ -13,9 +13,10 @@ module Paperclip
     def make
       dst = Tempfile.new([ @basename, @format ].compact.join("."))
       dst.binmode
-#ffmpeg -i INPUT -s 320x240 -r 30000/1001 -b 200k -bt 240k -vcodec libx264 -coder 0 -bf 0 -refs 1 -flags2 -wpred-dct8x8 -level 30 -maxrate 10M -bufsize 10M -acodec libfaac -ac 2 -ar 48000 -ab 192k OUTPUT.mp4
+#-i /yourfile.mov -y -f mp4 -vcodec libx264 -crf 28 -threads 0 -flags +loop -cmp +chroma -deblockalpha -1 -deblockbeta -1 -refs 3 -bf 3 -coder 1 -me_method hex -me_range 18 -subq 7 -partitions +parti4x4+parti8x8+partp8x8+partb8x8 -g 320 -keyint_min 25 -level 41 -qmin 10 -qmax 51 -qcomp 0.7 -trellis 1 -sc_threshold 40 -i_qfactor 0.71 -flags2 +mixed_refs+dct8x8+wpred+bpyramid -padcolor 000000 -padtop 0 -padbottom 0 -padleft 0 -padright 0 -acodec libfaac -ab 80kb -ar 48000 -ac 2 outputfile.mp4
 
-      cmd = %Q[ -i "#{File.expand_path(file.path)}" -f mov -b 720k -r 23.976 -aspect 16:9 -s 480x320 -acodec libfaac -ab 256k -ar 44100 -ac 2 -y ]
+      @mp4Options = " -y -acodec libfaac -ab 128k -f mp4 -vcodec libx264 -vpre fast -crf 22 -threads 2 "
+      cmd = %Q[ -i "#{File.expand_path(file.path)}" -y -acodec libfaac -ab 128k -f mp4 -vcodec libx264 -vpre fast -crf 22 -threads 2  ]
       cmd << %Q["#{File.expand_path(dst.path)}"]
 
       begin
