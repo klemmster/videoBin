1344a88a2ed3eec65b16ee718700fe956c0630db
Richard Klemm <klemmster@gmail.com>
2011-04-06 22:42:27 +0200

    Fix Admin Errors and Tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   before_filter :authenticate, :only => [:edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
-  before_filter :admin_user,   :only => :destroy
+  before_filter :admin_user,   :only => [:destroy]
 
 
   def index
@@ -32,21 +32,23 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user.destroy
+    User.find(params[:id]).destroy
     redirect_to users_path, :flash => { :success => "User destroyed." }
   end
 
   private
 
-
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
   end
+
   def admin_user
-    @user = User.find(params[:id])
-    redirect_to(root_path) if !current_user.admin? || current_user?(@user)
+    if not current_user
+      redirect_to(signin_path)
+    else
+      redirect_to(root_path) if !current_user.admin? || current_user?(@user)
+    end
   end
-  private
 
 end
